96261b9333fa329ac38ed6ad4bf2317386651c20
Oz-Smith <127737553+Oz-Smith@users.noreply.github.com>
2023-05-10 12:30:43 +0100

    Outline of the script needed

---
+++ Raster_Catalogue.py
@@ -0,0 +1,31 @@
+# import relevant files such as folio, geopandas etc
+
+# Create a list to store the GeoTIFF file paths
+
+# Define the directory to scan
+
+# Iterate through all files in the directory
+
+# Check if the file is a GeoTIFF
+
+# Get the full file path
+
+# Create an empty GeoDataFrame to store the metadata
+
+# Create a list to store the GeoTIFF file paths
+
+# Open the raster file
+
+# Create a polygon from the bounding box
+
+# Add the metadata to the GeoDataFrame
+
+# Create a map centred on the first GeoTIFF file
+
+# Add each GeoTIFF file to the map
+
+# Create a GeoJSON representation of the footprint
+
+# Add the footprint to the map as a GeoJSON overlay
+
+# Add a marker at the centroid of the footprint
