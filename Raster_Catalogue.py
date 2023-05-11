63411970bf0e0061ed25cac018a68bbe1636e294
Oz-Smith <127737553+Oz-Smith@users.noreply.github.com>
2023-05-11 15:42:33 +0100

    Update Raster_Catalogue.py
    Not happy with results - major change of direction which has now thrown up more bugs than before....not a happy bunny

--- Raster_Catalogue.py
+++ Raster_Catalogue.py
@@ -1,49 +1,59 @@
-# import relevant files such as folio, geopandas etc
-import os
+# import relevant package  folio, geopandas etc
+import glob, shapely
+import pandas as pdd
 import geopandas as gpd
+import rasterio as rio
+from rasterio import features # necessary as features often not resolved using an import of rasterio
 import folium
-from rasterio.plot import show
-from rasterio.mask import mask
 
-# Create a list to store the GeoTIFF file paths
-geotiff_paths = []
+# define a list of the GeoTIFF file paths
+def get_tif_paths(tif_directory: str) -> list[str]:
+    # Use glob and the .tif suffix to retrieve paths for all .tif files in the passed directory
+    return glob.glob(f'{tif_directory}/*.tif')
 
-# Define the directory to scan - this will need to be changed by each user
-directory = 'C:/Users/Oz Smith/Downloads'
+def get_tif_footprint(tif_path: str) -> gpd.GeoSeries:
+    with rio.open(tif_path) as img:
 
-# Iterate through all files in the directory
-for file_name in os.listdir(directory):
-    file_path = os.path.join(directory, file_name)
+        # Set all valid data cells to the same value (1) for feature extraction
+        tif_nodata = img.profile ['nodata']
+        tif_array = img.read(1)
+        tif_array[tif_array != tif_nodata] = 1
 
-# Check if the file is a GeoTIFF
-if file_path.endswith('.tif'):
-    # get the full file path
-    geotiff_paths.append(file_path)
+        #create a mask of invalid data cells to ignore in feature extraction
+        tif_mask = img.dataset_mask()
 
-# Create an empty GeoDataFrame to store the metadata
-metadata = gpd.GeoDataFrame()
+        # Iterate over features (contiguous areas of the same value), of which there should be one (in typical satellite imagery)
+        for geom, val in features.shapes(
+                tif_array, mask=tif_mask, transform=img.transform
+        ):
 
-# iterate through all GeoTIFF files
-for path in geotiff_paths:
-    # open each ratser file
-    with rasterio.open(path) as src:
-        # create a polygon from the bounding box
-        bbox = src.bounds
-        poly = gpd.GeoDataFrame({'geometry': gpd.GeoSeries(box(*bbox))}, index=[0], crs=src.crs)
-        # Add the metadata to the GeoDataFrame
-        meta = src.meta
-        meta.update({'path': path, 'bbox': bbox})
-        metadata = metadata.append(meta, ignore_index=True)
+            # Create Shapely Polygon of the feature
+            geom = shapely.Polygon(geom['coordinates'][0])
 
-# Create a map centred on the first GeoTIFF file
-        if path == geotiff_paths[0]:
-             m = folium.Map(location=[poly.gemoetry.centroid.y, poly.geometry.centroid.x], zoom_start=12)
-# Add each GeoTIFF file to the map
+            # Create GeoSeries of the feature and reproject to WGS 84
+            footprint_gs = gpd.GeoSeries(
+                data=geom,
+                crs=img.crs
+            ).to_crs(
+                'EPSG:4326'
+            )
 
-# Create a GeoJSON representation of the footprint
+        return footprint_gs
+def build_footprint_gdf(gs_array: list[gpd.GeoSeries]) -> gpd.GeoDataFrame:
+     # Build a GeoDataFrame from a concatenated GeoSeries of those in the passed list
+     return gpd.GeoDataFrame(geometry=pd.concat(gs_array))
+def main():
+     ''
 
-# Add the footprint to the map as a GeoJSON overlay
+if __name__ == '__main__':
+     main()
+     tif_directory = r'C:\Users\Oz Smith\Downloads\dimapV2_PHR1A_acq20230510_deld6b99b48\IMG_PHR1A_PMS_001'
+     tif_paths = get_tif_paths(tif_directory)
+     gs_array = []
 
-# Add a marker at the centroid of the footprint
+     for path in tif_paths:
+         gs_array.append(get_tif_footprint(path))
 
-# Display the map
+     gdf = build_footprint_gdf(gs_array)
+
+     print(gdf.centroid)
