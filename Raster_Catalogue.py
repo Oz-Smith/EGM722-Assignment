550ba982235a13c5bc8cf82d76ce283e4c0bfaee
Oz-Smith <127737553+Oz-Smith@users.noreply.github.com>
2023-05-15 13:01:47 +0100

    debugging
    Debugged and some more code added for file paths to be displayed

--- Raster_Catalogue.py
+++ Raster_Catalogue.py
@@ -5,12 +5,19 @@ import folium
 import geopandas as gpd
 import pandas as pd
 import rasterio as rio
-from rasterio import features  # necessary as features often not resolved using an import of rasterio
+import os
 
-# define a list of the GeoTIFF file paths
+from rasterio import features  # Necessary as features often not resolved using an import of rasterio
+
+# Define a list of the GeoTIFF file paths
 def get_tif_paths(tif_directory: str) -> list[str]:
-    # Use glob and the .tif suffix to retrieve paths for all .tif files in the passed directory
-    return glob.glob(f'{tif_directory}/*.tif')
+    # Use os.walk to search for all .tif suffix to retrieve paths for all .tif files in the passed directory
+    tif_paths = []
+    for root, dirs, files in os.walk(tif_directory):
+        for file in files:
+            if file.endswith(".tif"):
+                tif_paths.append(os.path.join(root, file))
+    return tif_paths
 
 def get_tif_footprint(tif_path: str) -> gpd.GeoSeries:
     with rio.open(tif_path) as img:
@@ -20,10 +27,10 @@ def get_tif_footprint(tif_path: str) -> gpd.GeoSeries:
         tif_array = img.read(1)
         tif_array[tif_array != tif_nodata] = 1
 
-        #create a mask of invalid data cells to ignore in feature extraction
+        # Create a mask of invalid data cells to ignore in feature extraction
         tif_mask = img.dataset_mask()
 
-        # Iterate over features (contiguous areas of the same value), of which there should be one (in typical satellite imagery)
+        # Iterate over features (contiguous areas of the same value), of which there should be one
         for geom, val in features.shapes(
                 tif_array, mask=tif_mask, transform=img.transform
         ):
@@ -40,30 +47,37 @@ def get_tif_footprint(tif_path: str) -> gpd.GeoSeries:
             )
 
         return footprint_gs
-def build_footprint_gdf(gs_array: list[gpd.GeoSeries]) -> gpd.GeoDataFrame:
-     # Build a GeoDataFrame from a concatenated GeoSeries of those in the passed list
-     return gpd.GeoDataFrame(geometry=pd.concat(gs_array))
+
+    # Debug add tiff paths to the map
+def build_footprint_gdf(gs_array: list[gpd.GeoSeries], tif_paths: list[str]) -> gpd.GeoDataFrame:
+    # Build a GeoDataFrame from a concatenated GeoSeries of those in the passed list
+    gdf = gpd.GeoDataFrame(geometry=pd.concat(gs_array))
+
+    # Add colum for file paths
+    gdf['file_path'] = tif_paths
+
+    return gdf
 def main():
      ''
 
 if __name__ == '__main__':
      main()
-     tif_directory = r'C:\Users\Oz Smith\Downloads' # This will need to be changed for each user
+     tif_directory = r"C:\Users\Oz Smith\Downloads"
      tif_paths = get_tif_paths(tif_directory)
      gs_array = []
 
      for path in tif_paths:
          gs_array.append(get_tif_footprint(path))
 
-     gdf = build_footprint_gdf(gs_array)
+     gdf = build_footprint_gdf(gs_array, tif_paths)
 
-     # Debug - Reproject the geometry to a projected CRS before getting its centroid
+    # Debug Reproject the geometry to a projected CRS before getting its centroid
 
-     # Create a folium map centred on the centroid of the GeoDataFrame
+    # Create a folium map centred on the centroid of the GeoDataFrame
      map_center = list(gdf.centroid.iloc[0].coords[0][::-1])
      m = folium.Map(location=map_center, zoom_start=13)
 
-     # Add GeoDataFrame to the map as a layer with blue fill colour and black border
+    # Add GeoDataFrame to the map as a layer with blue fill colour and black border
      folium.GeoJson(
          gdf,
          name='Footprints',
@@ -72,9 +86,9 @@ if __name__ == '__main__':
              'color': 'black',
              'weight': 2,
              'fillOpacity': 0.3
-         }
-     ).add_to(m)
-
+         },
+        tooltip=folium.features.GeoJsonTooltip(fields=['file_path']) # Add file path to the layer
+).add_to(m)
 # Add layer control to the map
 folium.LayerControl().add_to(m)
 
