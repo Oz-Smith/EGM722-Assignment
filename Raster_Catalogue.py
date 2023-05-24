136426a8969f86713560e798765abf1e4e5cc70a
Oz-Smith <127737553+Oz-Smith@users.noreply.github.com>
2023-05-24 09:22:15 +0100

    Final Commit

--- Raster_Catalogue.py
+++ Raster_Catalogue.py
@@ -1,4 +1,5 @@
-# import relevant package  folio, geopandas etc
+# Import relevant package  folio, geopandas etc
+import os
 import glob
 import shapely
 import folium
@@ -6,20 +7,36 @@ import geopandas as gpd
 import pandas as pd
 import rasterio as rio
 import tqdm
-from rasterio import features  # Necessary as features often not resolved using an import of rasterio
+# Import features directly - necessary as module often not resolved through import of rasterio
+from rasterio import features
 
-# Define a list of the GeoTIFF file paths
+# Define a list of the GeoTIFF file paths and provide valid directory
 
 
-def get_tif_paths(tif_directory: str) -> list[str]:
+def get_tif_paths() -> list[str]:
+    # Enter infinite while loop until user provides valid directory containing >= 1 file with the .tif suffix
+    while True:
+        print('Enter valid directory in which to search for GeoTIFFs:')
+        tif_directory = input()
 
-    # Use glob instead of os walk to search for all .tif suffix to retrieve paths for all files in the directory
-    return glob.glob(f'{tif_directory}/**/*.tif', recursive=True)
+        # Verify the input directory exists
+        if not os.path.exists(tif_directory):
+            print('Directory does not exist.')
+            continue
+
+# Use glob instead of os walk to recursively search directory (and subdirectories) for files with the .tif suffix
+        tif_paths = glob.glob(f'{tif_directory}/**/*.tif', recursive=True)
+
+        # Verify that 1 or more files with the .tif suffix have been found
+        if len(tif_paths) == 0:
+            print(f'No files with .tif suffix found in {tif_directory}')
+            continue
+
+        return tif_paths
 
 
 def get_tif_footprint(tif_path: str) -> gpd.GeoSeries:
     with rio.open(tif_path) as img:
-
         # Set all valid data cells to the same value (1) for feature extraction
         tif_nodata = img.profile['nodata']
         tif_array = img.read(1)
@@ -32,7 +49,6 @@ def get_tif_footprint(tif_path: str) -> gpd.GeoSeries:
         for geom, val in features.shapes(
                 tif_array, mask=tif_mask, transform=img.transform
         ):
-
             # Create Shapely Polygon of the feature
             geom = shapely.Polygon(geom['coordinates'][0])
 
@@ -43,24 +59,27 @@ def get_tif_footprint(tif_path: str) -> gpd.GeoSeries:
             ).to_crs(
                 'EPSG:3857'
             )
+
         return footprint_gs
 
-    # Debug add tiff paths to the map
+# Debug add tiff paths to the map
 
 
 def build_footprint_gdf(gs_array: list[gpd.GeoSeries], tif_paths: list[str]) -> gpd.GeoDataFrame:
     # Build a GeoDataFrame from a concatenated GeoSeries of those in the list
     gdf = gpd.GeoDataFrame(geometry=pd.concat(gs_array))
 
-    # Add colum for file paths
+    # Add column for file paths to GeoDataFrame
     gdf['file_path'] = tif_paths
     return gdf
 
 
 def get_gdf_centroid(gdf: gpd.GeoDataFrame) -> list[int]:
-    gdf = gdf.to_crs('EPSG:3857')  # Reproject the geometries to a projected CRS
+    # Reproject the GeoDataFrame to EPSG:3857 to prevent GeoPandas centroid warning
+    gdf = gdf.to_crs('EPSG:3857')
 
-    gdf_centroid = gdf.dissolve().centroid.to_crs('EPSG:4326')  # Calculate the centroid
+    # Obtain GeoDataFrame centroid and reproject to WGS 84 for folium
+    gdf_centroid = gdf.dissolve().centroid.to_crs('EPSG:4326')
     return [gdf_centroid.iloc[0].y, gdf_centroid.iloc[0].x]
 
 
@@ -85,15 +104,13 @@ def build_folium_map(map_centre: list[int], gdf: gpd.GeoDataFrame):
     folium.LayerControl().add_to(folium_map)
     return folium_map
 
-# Define where to look for tiff files - directory will need to change for each user
 
-
+# Wrap processing functions in main() function for concise calling of entire workflow in other scripts
 def main():
-    tif_directory = r"Input your directory here"
-    tif_paths = get_tif_paths(tif_directory)
+    tif_paths = get_tif_paths()
     gs_array = []
 
-    # Build a progress bar as nothing seems to happen until the program finishes
+    # Use tqdm to show progress bar
     for path in tqdm.tqdm(tif_paths, desc='Retrieving GeoTIFF valid data footprints: '):
         gs_array.append(get_tif_footprint(path))
 
@@ -101,8 +118,11 @@ def main():
     gdf = build_footprint_gdf(gs_array, tif_paths)
     map_centre = get_gdf_centroid(gdf)
     folium_map = build_folium_map(map_centre, gdf)
+
+    # Save folium html map to script's working directory
     folium_map.save(r"GeoTIFF_footprints_map.html")
 
 
+# Ensure main() is only run when current file is executed directly
 if __name__ == '__main__':
     main()
